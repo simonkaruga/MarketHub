d358768352b96dba600b6bc3f3cc249bb11fbe82
simonkaruga <simonkaruganjoroge@gmail.com>
2026-01-12 15:56:40 +0300

    Add origin

---
+++ backend/app/services/__init__.py
@@ -0,0 +1,9 @@
+"""
+Services package
+Business logic and external integrations
+"""
+# Services will be added as we build:
+# from app.services import mpesa_service
+# from app.services import cloudinary_service
+# from app.services import email_service
+```
