44909a3aba1406ed2714fbb98caaf4c20e145a8b
RONNY <mboyaronny41@gmail.com>
2026-01-13 11:01:57 +0300

    models-init.py

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,12 +1,16 @@
+"""
 Database models package
 Models will be imported here as we create them
 """
 from app.models.user import User, UserRole
+from app.models.category import Category
+from app.models.product import Product
+from app.models.cart import Cart, CartItem
 
 # Other models will be added as we create them:
-# from app.models.product import Product
-# from app.models.category import Category
-# from app.models.cart import Cart, CartItem
 # from app.models.order import MasterOrder, SubOrder, OrderItem
+# from app.models.review import Review
+# from app.models.hub import Hub
+# from app.models.merchant_application import MerchantApplication
 
-__all__ = ['User', 'UserRole']
+__all__ = ['User', 'UserRole', 'Category', 'Product', 'Cart', 'CartItem']
