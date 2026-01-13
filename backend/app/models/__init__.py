d6f840302705d40dd835fc311af2b3f4fc7eed1c
RONNY <mboyaronny41@gmail.com>
2026-01-13 11:22:40 +0300

    models_init.py

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -6,11 +6,24 @@ from app.models.user import User, UserRole
 from app.models.category import Category
 from app.models.product import Product
 from app.models.cart import Cart, CartItem
+from app.models.hub import Hub
+from app.models.delivery_partner import DeliveryPartner
+from app.models.order import (
+    MasterOrder, SubOrder, OrderItem,
+    PaymentMethod, PaymentStatus, SubOrderStatus
+)
 
 # Other models will be added as we create them:
-# from app.models.order import MasterOrder, SubOrder, OrderItem
 # from app.models.review import Review
-# from app.models.hub import Hub
 # from app.models.merchant_application import MerchantApplication
 
-__all__ = ['User', 'UserRole', 'Category', 'Product', 'Cart', 'CartItem']
+__all__ = [
+    'User', 'UserRole',
+    'Category',
+    'Product',
+    'Cart', 'CartItem',
+    'Hub',
+    'DeliveryPartner',
+    'MasterOrder', 'SubOrder', 'OrderItem',
+    'PaymentMethod', 'PaymentStatus', 'SubOrderStatus'
+]
