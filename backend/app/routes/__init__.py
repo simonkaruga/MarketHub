8d490f76a756d85e608b7ca1fe70eef62b38bf21
sheilawuor <sheilawuor01@gmail>
2026-01-13 10:50:38 +0300

    add files

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -2,6 +2,6 @@
 API routes package
 Blueprints will be imported here as we create them
 """
-from app.routes import auth
+from app.routes import auth, categories, products, merchant, admin
 
-__all__ = ['auth']
+__all__ = ['auth', 'categories', 'products', 'merchant', 'admin']
